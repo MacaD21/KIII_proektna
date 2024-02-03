78fd7c7788c6d28fc99d76fdb21b0c128576ff40
Marija Dimitrova <marija.dimitrova.s3@gmail.com>
2024-02-03 15:51:37 +0100

    Modified Kubernetes

--- src/main/java/mk/ukim/finki/wp/lab/LabApplication.java
+++ src/main/java/mk/ukim/finki/wp/lab/LabApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 //@ServletComponentScan
 @SpringBootApplication
@@ -15,8 +14,8 @@ public class LabApplication {
         SpringApplication.run(LabApplication.class, args);
     }
 
-    @Bean
-    PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder(10);
-    }
+//    @Bean
+//    PasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder(10);
+//    }
 }
